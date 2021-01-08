7b87b7b1ad66c46447ae0e2bf5dacaa07482c1ea
CorentinBordes-CPE <61121078+CorentinBordes-CPE@users.noreply.github.com>
2021-01-08 14:20:06 +0100

    again ...

--- Librairie Microbit/testPostSensor.py
+++ Librairie Microbit/testPostSensor.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/api/updateSensor/'
+url = 'http://192.168.1.25:5000/api/updateSensor/'
 myobj = {'m_id': 42,'m_intensity': 42}
 print(myobj)
 x = requests.post(url, data = myobj)
