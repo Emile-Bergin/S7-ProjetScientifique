1014a0d6a65b23e4f3f6562e46908190f9bc47d1
emile bergin <berginemile098@gmail.com>
2021-01-12 21:29:03 +0100

    java jar

--- Application_Module/EmmergencyManager/src/main/java/com/SuperCompany/Mode.java
+++ Application_Module/EmmergencyManager/src/main/java/com/SuperCompany/Mode.java
@@ -2,7 +2,7 @@ package com.SuperCompany;
 
 public class Mode {
     private static boolean DEBUG = true;
-    public static boolean USEREELAPI = false;
+    public static boolean USEREELAPI = true;
 
     public static void println(String str){
         if (DEBUG){
